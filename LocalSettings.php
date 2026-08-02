487fd8ffbbe02a04ec923388cf8a7a86ad755d90
Patrick Nance <jpnance@gmail.com>
2026-08-02 10:23:16 -0700

    Disable these special pages altogether.

--- LocalSettings.php
+++ LocalSettings.php
@@ -71,3 +71,8 @@ $wgGroupPermissions['user']['edit'] = false;
 $wgGroupPermissions['yosher']['edit'] = true;
 
 $wgShowExceptionDetails = true;
+
+$wgDisabledSpecialPages = [
+	'recentchanges',
+	'recentchangeslinked',
+];
